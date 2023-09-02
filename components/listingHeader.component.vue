9f2eaa9dfd6d15cf24b0acb13f2dcabb6a3f3ec1
Kamil Nakielny <kamil.nakielny@gmail.com>
2023-09-02 20:03:28 +0200

    update

--- components/listingHeader.component.vue
+++ components/listingHeader.component.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
-    <div style="background:url(https://img.geocaching.com:443/1c75def2-54cb-4ce8-9e09-dcc22042d151.png); border:2px solid black; color:white; margin-bottom:30px; padding:30px">
-      <h3 style="text-align: center">
-        <strong>
-          <span style="color: white; cursor: pointer" @click="routeTo('PL')">Język Polski</span> |
-          <span style="color: white; cursor: pointer" @click="routeTo('EN')">English</span> |
-          <span style="color: white; cursor: pointer" @click="routeTo('UA')">Українська мова</span> |
-          <span style="color: white; cursor: pointer" @click="routeTo('DE')">Deutsch</span> |
-          <span style="color: white; cursor: pointer" @click="routeTo('CZ')">Če&scaron;tina</span>
-        </strong>
-      </h3>
+    <div class="language-box">
+        <NuxtLink to="/ListingPL" class="link">
+          Język Polski
+        </NuxtLink> |
+
+        <NuxtLink to="/ListingEN" class="link">
+          English
+        </NuxtLink> |
+
+        <NuxtLink to="/ListingUA" class="link">
+          Українська мова
+        </NuxtLink> |
+
+        <NuxtLink to="/ListingDE" class="link">
+          Deutsch
+        </NuxtLink> |
+
+        <NuxtLink to="/ListingCZ" class="link">
+          Če&scaron;tina
+        </NuxtLink>
     </div>
 
-    <p style="text-align: center">
+    <div class="image-box">
       <img
-        src="https://img.geocaching.com:443/801b9bf1-ec97-4af5-bdd4-bf191efb6045.jpg"
+        src="~/assets/pieskowa-skala.jpg"
         alt="PS"
       />
-    </p>
+    </div>
   </div>
 </template>
 
@@ -25,10 +35,32 @@
 
 export default {
   name: "ListingHeader",
-  methods: {
-    routeTo(listingVersion) {
-      // router.push(`/listing-${listingVersion}`);
-    },
-  },
 };
 </script>
+
+<style lang="scss">
+.language-box {
+  background:url(https://img.geocaching.com:443/1c75def2-54cb-4ce8-9e09-dcc22042d151.png);
+  border:2px solid black;
+  color:white;
+  margin-bottom:30px;
+  padding:30px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.link {
+  color: white;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.image-box {
+  text-align: center;
+}
+
+a {
+  color: white !important;
+}
+</style>
